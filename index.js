6b9e7679dc5ad03243cb5306c5b66272510cad3c
shumailmehmood <shumailmehmood1@gmail.com>
2023-07-22 14:40:00 +0500

    Configure 6

--- index.js
+++ index.js
@@ -27,7 +27,20 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    next(createError(404));
+    res.send("404 Error caught");
+});
+// error handler
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get("env") === "production" ? err : {};
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+});
 
 app.get('/', (req, res) => {
     res.send('Hey this is my API running 🥳')
