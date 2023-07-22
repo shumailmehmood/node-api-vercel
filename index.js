6670ed217e3bbfd425ff0ec6079cf185d7c0a145
shumailmehmood <shumailmehmood1@gmail.com>
2023-07-22 14:43:24 +0500

    Configure 8

--- index.js
+++ index.js
@@ -28,10 +28,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 // catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    next(createError(404));
-    res.send("404 Error caught");
-});
+// app.use(function (req, res, next) {
+//     next(createError(404));
+//     res.send("404 Error caught");
+// });
 // error handler
 // app.use(function (err, req, res, next) {
 //     // set locals, only providing error in development
